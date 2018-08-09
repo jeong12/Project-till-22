fdffe7d56f05ac4d674aeabc851f252c36470135
Jeong <hee2012j@naver.com>
2018-08-09 19:41:25 +0900

    파일추가

--- src/com/bitcamp/project/kyr/DAO/screenDAO.java
+++ src/com/bitcamp/project/kyr/DAO/screenDAO.java
@@ -1,6 +1,9 @@
 package com.bitcamp.project.kyr.DAO;
 
 public class screenDAO {
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
 
 	public insert()
 	{
@@ -14,4 +17,58 @@ public class screenDAO {
 	{
 		  
 	}
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> branch 'master' of https://github.com/jeong12/Project-till-22.git
+
+	/*public insert()
+	{
+		
+	}
+	public rent()
+	{
+		
+	}
+	public retu()
+	{
+		
+		
+	}
+	*/
+<<<<<<< HEAD
+
+=======
+=======
+  
+	/*public insert()
+	{
+		
+	}
+	public rent()
+	{
+		
+	}
+	public retu()
+	{
+		
+	}*/
+>>>>>>> branch 'master' of https://github.com/jeong12/Project-till-22.git
+=======
+  
+	/*public insert()
+	{
+		
+	}
+	public rent()
+	{
+		
+	}
+	public retu()
+	{
+		
+	}*/
+>>>>>>> branch 'master' of https://github.com/jeong12/Project-till-22.git
+>>>>>>> branch 'master' of https://github.com/jeong12/Project-till-22.git
+>>>>>>> branch 'master' of https://github.com/jeong12/Project-till-22.git
 }
