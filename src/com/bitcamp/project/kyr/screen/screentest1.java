82b582edb603c1f4a8ea425f62c605b0f185c003
kim <chfaos120@naver.com>
2018-08-09 12:15:00 +0900

    파일 생성

---
+++ src/com/bitcamp/project/kyr/screen/screentest1.java
@@ -0,0 +1,13 @@
+package com.bitcamp.project.kyr.screen;
+
+public class screentest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//dsfsdfsdaf/
+		//sdfsdf
+		//sdfsdf
+		
+	}
+
+}
