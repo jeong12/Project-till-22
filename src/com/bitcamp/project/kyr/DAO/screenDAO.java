7da31b3070105b490a3da01a6a146d763e34e528
kim <chfaos120@naver.com>
2018-08-09 18:28:02 +0900

    kyr.exerd

---
+++ src/com/bitcamp/project/kyr/DAO/screenDAO.java
@@ -0,0 +1,17 @@
+package com.bitcamp.project.kyr.DAO;
+
+public class screenDAO {
+
+	public insert()
+	{
+		
+	}
+	public rent()
+	{
+		
+	}
+	public retu()
+	{
+		
+	}
+}
--- src/com/bitcamp/project/kyr/screen/screentest1.java
+++ src/com/bitcamp/project/kyr/screen/screentest1.java
@@ -4,9 +4,7 @@ public class screentest1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//dsfsdfsdaf/
-		//sdfsdf 
-		//sdfsdf
+		
 		
 	}
 
