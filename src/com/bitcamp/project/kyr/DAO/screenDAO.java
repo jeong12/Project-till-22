18948bdbbb3ef196dcfd5c3fdf06262dbd2236d6
kim <chfaos120@naver.com>
2018-08-09 19:00:36 +0900

    ffff

--- src/com/bitcamp/project/kyr/DAO/screenDAO.java
+++ src/com/bitcamp/project/kyr/DAO/screenDAO.java
@@ -1,8 +1,6 @@
 package com.bitcamp.project.kyr.DAO;
 
 public class screenDAO {
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 	/*public insert()
 	{
@@ -18,34 +16,5 @@ public class screenDAO {
 		
 	}
 	*/
-=======
-  
-	/*public insert()
-	{
-		
-	}
-	public rent()
-	{
-		
-	}
-	public retu()
-	{
-		
-	}*/
->>>>>>> branch 'master' of https://github.com/jeong12/Project-till-22.git
-=======
-  
-	/*public insert()
-	{
-		
-	}
-	public rent()
-	{
-		
-	}
-	public retu()
-	{
-		
-	}*/
->>>>>>> branch 'master' of https://github.com/jeong12/Project-till-22.git
+
 }
