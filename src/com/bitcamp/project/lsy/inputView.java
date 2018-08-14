c3da42d1244dc5c33201c62da4330867006a3e3c
lee <kei_romance@naver.com>
2018-08-14 20:38:26 +0900

    화면변경

--- src/com/bitcamp/project/lsy/inputView.java
+++ src/com/bitcamp/project/lsy/inputView.java
@@ -11,12 +11,10 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.JList;
+import javax.swing.JComboBox;
 
 public class inputView extends JFrame implements ActionListener {
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
 
 	/**
 	 * Launch the application.
@@ -43,44 +41,25 @@ public class inputView extends JFrame implements ActionListener {
 		getContentPane().setLayout(null);
 		
 		JLabel morningLabel = new JLabel("아침");
-		morningLabel.setBounds(71, 30, 40, 21);
+		morningLabel.setBounds(30, 30, 40, 21);
 		getContentPane().add(morningLabel);
 		
-		textField = new JTextField();
-		textField.setBounds(146, 30, 192, 21);
-		getContentPane().add(textField);
-		textField.setColumns(10);
-		
 		JLabel lunchLabel = new JLabel("점심");
-		lunchLabel.setBounds(71, 86, 40, 21);
+		lunchLabel.setBounds(30, 86, 40, 21);
 		getContentPane().add(lunchLabel);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(146, 86, 192, 21);
-		getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-		
 		JLabel dinnerLabel = new JLabel("저녁");
-		dinnerLabel.setBounds(71, 142, 40, 21);
+		dinnerLabel.setBounds(30, 142, 40, 21);
 		getContentPane().add(dinnerLabel);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(146, 142, 192, 21);
-		getContentPane().add(textField_2);
-		textField_2.setColumns(10);
-		
 		JLabel foodLabel = new JLabel("간식");
-		foodLabel.setBounds(71, 198, 40, 21);
+		foodLabel.setBounds(30, 198, 40, 21);
 		getContentPane().add(foodLabel);
-		
-		textField_3 = new JTextField();
-		textField_3.setBounds(146, 198, 192, 21);
-		getContentPane().add(textField_3);
-		textField_3.setColumns(10);
 		
 		JButton inputButton = new JButton("입력");
 		inputButton.setBounds(30, 326, 394, 55);
 		getContentPane().add(inputButton);
+		inputButton.addActionListener(new saveDTO());
 		
 		JButton deleteButton = new JButton("초기화");
 		deleteButton.setBounds(30, 432, 102, 60);
@@ -89,6 +68,54 @@ public class inputView extends JFrame implements ActionListener {
 		JButton mainButton = new JButton("메인으로");
 		mainButton.setBounds(322, 432, 102, 60);
 		getContentPane().add(mainButton);
+		
+		JComboBox morBox1 = new JComboBox();
+		morBox1.setBounds(82, 30, 82, 21);
+		getContentPane().add(morBox1);
+		
+		JComboBox morBox2 = new JComboBox();
+		morBox2.setBounds(200, 30, 82, 21);
+		getContentPane().add(morBox2);
+		
+		JComboBox morBox3 = new JComboBox();
+		morBox3.setBounds(322, 30, 82, 21);
+		getContentPane().add(morBox3);
+		
+		JComboBox lunBox1 = new JComboBox();
+		lunBox1.setBounds(82, 86, 82, 21);
+		getContentPane().add(lunBox1);
+		
+		JComboBox lunBox2 = new JComboBox();
+		lunBox2.setBounds(200, 86, 82, 21);
+		getContentPane().add(lunBox2);
+		
+		JComboBox lunBox3 = new JComboBox();
+		lunBox3.setBounds(322, 86, 82, 21);
+		getContentPane().add(lunBox3);
+		
+		JComboBox diBox1 = new JComboBox();
+		diBox1.setBounds(82, 142, 82, 21);
+		getContentPane().add(diBox1);
+		
+		JComboBox diBox2 = new JComboBox();
+		diBox2.setBounds(200, 142, 82, 21);
+		getContentPane().add(diBox2);
+		
+		JComboBox diBox3 = new JComboBox();
+		diBox3.setBounds(322, 142, 82, 21);
+		getContentPane().add(diBox3);
+		
+		JComboBox foodBox1 = new JComboBox();
+		foodBox1.setBounds(82, 198, 82, 21);
+		getContentPane().add(foodBox1);
+		
+		JComboBox foodBox2 = new JComboBox();
+		foodBox2.setBounds(200, 198, 82, 21);
+		getContentPane().add(foodBox2);
+		
+		JComboBox foodBox3 = new JComboBox();
+		foodBox3.setBounds(322, 198, 82, 21);
+		getContentPane().add(foodBox3);
 		mainButton.addActionListener(new ActionListener() {
 			
 			@Override
@@ -106,5 +133,4 @@ public class inputView extends JFrame implements ActionListener {
 		inputView frame = new inputView();
 		frame.setVisible(true);
 	}
-
 }
---
+++ src/com/bitcamp/project/lsy/saveDTO.java
@@ -0,0 +1,14 @@
+package com.bitcamp.project.lsy;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class saveDTO implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
