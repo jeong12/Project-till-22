e2b876737abd77a936d9b33b863bdbdbe9b6e08c
kim <chfaos120@naver.com>
2018-08-23 20:17:00 +0900

    46654

--- src/com/bitcamp/project/kyr/DAO/screenDAO.java
+++ src/com/bitcamp/project/kyr/DAO/screenDAO.java
@@ -198,18 +198,18 @@ public class screenDAO {
 					TimeZone jst =TimeZone.getTimeZone("Asia/Seoul");
 					Calendar Cal = Calendar.getInstance(jst); 
 
-					sbl.append(" insert into pro3_retu(bnum,number,rdate) values (?,?,now()) ");
-					pstmt=conn.prepareStatement(sbl.toString());
-					pstmt.setFloat(1, dto.getBnum());
-					pstmt.setInt(2, dto.getNumber());
-					r=pstmt.executeUpdate();
+					sb2.append(" delete from pro3_rent where round(bnum,2) = round(?,2) ");
+					pstmt2=conn.prepareStatement(sb2.toString());
+					pstmt2.setFloat(1, dto.getBnum());
+					r2=pstmt2.executeUpdate();
 
-					if(r>0) {
-						sb2.append(" delete from pro3_rent where round(bnum,2) = round(?,2) ");
-						pstmt2=conn.prepareStatement(sb2.toString());
-						pstmt2.setFloat(1, dto.getBnum());
-						r2=pstmt2.executeUpdate();
-						if(r2>0)
+					if(r2>0) {
+						sbl.append(" insert into pro3_retu(bnum,number,rdate) values (?,?,now()) ");
+						pstmt=conn.prepareStatement(sbl.toString());
+						pstmt.setFloat(1, dto.getBnum());
+						pstmt.setInt(2, dto.getNumber());
+						r=pstmt.executeUpdate();
+						if(r>0)
 						{
 							sb3.append(" insert into pro3_book(bname,publish,redate,bnum) ");
 							sb3.append(" select bname,publish,redate,bnum ");
