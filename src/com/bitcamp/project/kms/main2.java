220ec6104d18cb7caa42a060a3d4cca4fa2b6973
minsung2da <minsung2da@naver.com>
2018-08-23 17:29:16 +0900

    수정

--- src/com/bitcamp/project/kms/main2.java
+++ src/com/bitcamp/project/kms/main2.java
@@ -6,6 +6,11 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import com.bitcamp.project.khj.FirstView;
+import com.bitcamp.project.kyr.screen.mainscreen;
+import com.bitcamp.project.lsy.mainView;
+
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -47,15 +52,47 @@ public class main2 extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnNewButton = new JButton("New button");
+		JButton btnNewButton = new JButton("±è¿µ·Ï");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				int number = dto.getNumber();
+				
+				mainscreen ms = new mainscreen(number);
+				ms.setVisible(true);
+				
+			}
+		});
 		btnNewButton.setBounds(37, 54, 97, 57);
 		contentPane.add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("New button");
+		JButton btnNewButton_1 = new JButton("±èÈñÁ¤");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				int number = dto.getNumber();
+				
+				FirstView fv = new FirstView(number);
+				fv.setVisible(true);
+				
+				
+			}
+		});
 		btnNewButton_1.setBounds(146, 54, 97, 57);
 		contentPane.add(btnNewButton_1);
 		
-		JButton btnNewButton_2 = new JButton("New button");
+		JButton btnNewButton_2 = new JButton("ÀÌ¼¼¿µ");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String id = dto.getId();
+			    
+				mainView mv = new mainView();
+				
+				mv.setVisible(true);
+				
+			}
+		});
 		btnNewButton_2.setBounds(255, 54, 97, 57);
 		contentPane.add(btnNewButton_2);
 		
@@ -66,12 +103,19 @@ public class main2 extends JFrame {
 		      Update u = new Update(dto);
 		      u.setVisible(true);
 				
-			}
+			 }
 		});
 		btnNewButton_3.setBounds(80, 194, 109, 23);
 		contentPane.add(btnNewButton_3);
 		
 		JButton btnNewButton_4 = new JButton("È¸¿øÅ»Åð");
+		btnNewButton_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				Delete d = new Delete(dto);
+				d.setVisible(true);
+			}
+		});
 		btnNewButton_4.setBounds(201, 194, 109, 23);
 		contentPane.add(btnNewButton_4);
 	}
